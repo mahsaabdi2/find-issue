c43d31299213e123ebcb61f69605e8c9cd2ad7b9
Azalia <mahsa.abdi2@gmail.com>
2025-05-15 20:57:50 +0330

    styles in navbar by classnames

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,7 +1,12 @@
+'use client'
+
 import Link from "next/link"
 import {AiFillBug} from "react-icons/ai"
-
+import {usePathname} from "next/navigation"
+import classNames from "classnames"
 const Navbar = () => {
+
+    const currentPath=usePathname();
 
     const links=[
         {label:'Dashboard' , href:"/"},
@@ -14,7 +19,11 @@ const Navbar = () => {
       <ul className="flex  space-x-6">
         {links.map(link=>
         <Link key={link.href}
-         className="text-zinc-500 hover:text-zinc-800 transition-colors"
+         className={classNames({
+            'text-zink-900':link.href===currentPath,
+            'text-zinc-500':link.href !== currentPath,
+            'hover:text-zinc-800 transition-colors':true
+         })}
           href={link.href}>{link.label}</Link>)}
       
         </ul>
---
+++ app/issues/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const IssuesPage = () => {
+  return (
+    <div>
+      issuesPage
+    </div>
+  )
+}
+
+export default IssuesPage 
+
